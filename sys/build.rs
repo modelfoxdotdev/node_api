2dbdb7341aae10255146de19e00a509dcea49ee5
David Yamnitsky <david@yamnitsky.com>
2022-02-24 13:44:29 -0500

    handle error in build.rs correctly

--- sys/build.rs
+++ sys/build.rs
@@ -1,25 +1,22 @@
 fn main() {
 	match std::env::var("NODE_API_WINDOWS_X64_IMPORT_LIBRARY") {
 		Ok(path) => {
-			// set path
 			println!("cargo:rustc-link-search={}", path);
 			println!("cargo:rustc-link-lib=node");
 		}
-		Err(_) => {
-			// It's not set, download it.
-			windows_setup()
+		Err(std::env::VarError::NotPresent) => {
+			let version = "v16.4.0";
+			let arch = "x64";
+			let url = format!("https://nodejs.org/dist/{version}/win-{arch}/node.lib");
+			let node_lib_bytes = reqwest::blocking::get(url).unwrap().bytes().unwrap();
+			let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+			let node_lib_path = out_dir.join("node.lib");
+			std::fs::write(node_lib_path, node_lib_bytes).unwrap();
+			println!("cargo:rustc-link-search={}", out_dir.to_str().unwrap());
+			println!("cargo:rustc-link-lib=node");
+		}
+		Err(e) => {
+			panic!(e);
 		}
 	}
-}
-
-fn windows_setup() {
-	let version = "v16.4.0";
-	let arch = "x64";
-	let url = format!("https://nodejs.org/dist/{version}/win-{arch}/node.lib");
-	let node_lib_bytes = reqwest::blocking::get(url).unwrap().bytes().unwrap();
-	let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-	let node_lib_path = out_dir.join("node.lib");
-	std::fs::write(node_lib_path, node_lib_bytes).unwrap();
-	println!("cargo:rustc-link-search={}", out_dir.to_str().unwrap());
-	println!("cargo:rustc-link-lib=node");
 }
