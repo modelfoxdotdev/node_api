b0f17a64b7736f729de0e5b47ac7e3026343d517
David Yamnitsky <david@yamnitsky.com>
2022-02-24 11:24:02 -0500

    fix serde feature name and bump version

--- sys/build.rs
+++ sys/build.rs
@@ -1,18 +1,13 @@
 fn main() {
 	let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-	match target_os.as_str() {
-		"macos" => {
-			macos_setup();
-		}
-		"windows" => {
-			windows_setup();
-		}
-		_ => {}
-	};
+	if target_os == "macos" {
+		macos_setup();
+	} else if target_os == "windows" {
+		windows_setup();
+	}
 }
 
 fn macos_setup() {
-	println!("cargo:rustc-cdylib-link-arg=-Wl");
 	println!("cargo:rustc-cdylib-link-arg=-undefined");
 	println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
 }
@@ -20,11 +15,7 @@ fn macos_setup() {
 fn windows_setup() {
 	let version = "v16.4.0";
 	let arch = "x64";
-	let url = format!(
-		"https://nodejs.org/dist/{version}/win-{arch}/node.lib",
-		version = version,
-		arch = arch
-	);
+	let url = format!("https://nodejs.org/dist/{version}/win-{arch}/node.lib");
 	let node_lib_bytes = reqwest::blocking::get(url).unwrap().bytes().unwrap();
 	let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
 	let node_lib_path = out_dir.join("node.lib");
