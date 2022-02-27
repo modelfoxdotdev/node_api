a94330696b1861ddea47b6789adff1528fa69871
David Yamnitsky <david@yamnitsky.com>
2022-02-27 12:15:39 -0500

    fix clippy

--- error.rs
+++ error.rs
@@ -31,29 +31,28 @@ impl Error {
 	}
 
 	/// Create a `NodeAPIError` value from the most recent Node-API call.
-	pub fn from_last_node_api_error(env: napi_env, status: napi_status) -> Error {
-		unsafe {
-			let mut error_info = std::mem::MaybeUninit::uninit();
-			let last_error_status = napi_get_last_error_info(env, error_info.as_mut_ptr());
-			if last_error_status != napi_status::napi_ok {
-				napi_fatal_error(std::ptr::null(), 0, std::ptr::null(), 0);
-				unreachable!()
-			}
-			let error_info = error_info.assume_init();
-			let message = (*error_info).error_message;
-			let message = if !message.is_null() {
-				match CStr::from_ptr(message).to_str() {
-					Ok(message) => message,
-					Err(_) => {
-						napi_fatal_error(std::ptr::null(), 0, std::ptr::null(), 0);
-						unreachable!()
-					}
-				}
-			} else {
-				"empty error message"
-			};
-			let message = message.to_owned();
-			Error::NodeApi(status, message)
+	/// # Safety
+	pub unsafe fn from_last_node_api_error(env: napi_env, status: napi_status) -> Error {
+		let mut error_info = std::mem::MaybeUninit::uninit();
+		let last_error_status = napi_get_last_error_info(env, error_info.as_mut_ptr());
+		if last_error_status != napi_status::napi_ok {
+			napi_fatal_error(std::ptr::null(), 0, std::ptr::null(), 0);
+			unreachable!()
 		}
+		let error_info = error_info.assume_init();
+		let message = (*error_info).error_message;
+		let message = if !message.is_null() {
+			match CStr::from_ptr(message).to_str() {
+				Ok(message) => message,
+				Err(_) => {
+					napi_fatal_error(std::ptr::null(), 0, std::ptr::null(), 0);
+					unreachable!()
+				}
+			}
+		} else {
+			"empty error message"
+		};
+		let message = message.to_owned();
+		Error::NodeApi(status, message)
 	}
 }
